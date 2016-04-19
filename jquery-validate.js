df9c216b566bdbb4b149b766c84e2efb41188b86
Paweł Albecki <pawel.albecki@gmail.com>
2016-04-20 00:48:12 +0200

    email validate

--- jquery-validate.js
+++ jquery-validate.js
@@ -1,20 +1,36 @@
+/* globals jQuery */
 (function ( $ ) {
 
-  $.fn.validateText = function( options ) {
-    var settings = $.extend({
-      pattern: ".*",
-    }, options );
-    $(this).blur(function() {
-      if(!settings.pattern.test($(this).val())){
-        $("#error").text($(this).val() + " nie pasuje do: " + settings.pattern);
-        $(this).css({'background-color': 'red'});
-      }else{
-        $("#error").text("");
-        $(this).css({'background-color': ''});
-
-      }
-    });
-    return this;
+  $.fn.validateText = function( options, type ) {
+    if(type !== "email"){
+      return this.each(function () {
+        var settings = $.extend({
+          pattern: ".*",
+        }, options );
+        $(this).blur(function() {
+          if(!settings.pattern.test($(this).val())){
+            $("#error").text($(this).val() + " nie pasuje do: " + settings.pattern);
+            $(this).css({'background-color': 'red'});
+          }else{
+            $("#error").text("");
+            $(this).css({'background-color': ''});
+          }
+        });
+      });
+    }else{
+      return this.each(function () {
+        $(this).blur(function() {
+          if(!new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").test($(this).val())){
+            $("#error").text($(this).val() + " nie jest emailem");
+            $(this).css({'background-color': 'red'});
+          }else{
+            $("#error").text("");
+            $(this).css({'background-color': ''});
+          }
+        });
+      });
+    }
   };
+
 
 }( jQuery ));
--- validate.js
+++ validate.js
@@ -2,6 +2,6 @@ $(document).ready(function (){
 
 $('#imie').validateText( {pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłóńśźż]*$/} );
 $('#telefon').validateText( {pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłóńśźż]*$/} );
-$('#email').validateText( {pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłóńśźż]*$/} );
+$('#email').validateText( {pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłóńśźż]*$/}, 'email' );
 });
 
